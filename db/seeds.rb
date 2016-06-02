cb8f2c7b14b5151d1f17ba855967334fe8f2f1af
Christopher Eaton <eaton.cw@gmail.com>
2016-06-02 11:18:19 -0700

    updated seeds db for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Product.create(:name => "seed gen", :description => "seed gen product", :image_url => "http://fyf.tac-cdn.net/images/products/large/FYF-870.jpg", :colour => "red")
-Product.create(:name => "bazkets", :description => "huh?", :image_url => "https://www.independentfloralnetwork.com/profiles/abloomingbasketflowersandgifts/products/fruit%20baskets/fruitbasket.jpg", :colour => "blue")
-Product.create(:name => "new", :description => "yes", :image_url => "https://www.independentfloralnetwork.com/profiles/abloomingbasketflowersandgifts/products/fruit%20baskets/fruitbasket.jpg", :colour => "black")
+Product.create(:name => "basket 1", :description => "nice basket", :image_url => "http://fyf.tac-cdn.net/images/products/large/FYF-870.jpg", :colour => "red", :size => "big", :price => "22.56")
+Product.create(:name => "basket 2", :description => "great priced basket", :image_url => "https://www.independentfloralnetwork.com/profiles/abloomingbasketflowersandgifts/products/fruit%20baskets/fruitbasket.jpg", :colour => "blue", :size => "big", :price => "12.05")
+Product.create(:name => "basket 3", :description => "stylish basket", :image_url => "https://www.independentfloralnetwork.com/profiles/abloomingbasketflowersandgifts/products/fruit%20baskets/fruitbasket.jpg", :colour => "black", :size => "big", :price => "88.99")
 
