b8b4a6583d7969f5f299da2705d8f6e8f7255fd7
Christopher Eaton <eaton.cw@gmail.com>
2016-06-30 15:34:43 -0700

    updated mailer

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "eaton.cw@gmail.com"
+  layout 'mailer'
+end
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+	def contact_form
+	UserMailer.contact_form("eaton.cw@gmail.com", "Chris", "Hello World!")
+	end
+
+
+end
