b207b69ab8c7052d762445a2d1f2c271b9aeca75
Christopher Eaton <eaton.cw@gmail.com>
2016-06-30 13:27:44 -0700

    updated with contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,4 +12,14 @@ class StaticPagesController < ApplicationController
 	def landing_page
 		@products = Product.limit(6)
 	end
+
+	def thank_you
+		@name = params[:name]
+		@email = params[:email]
+		@message = params[:message]
+		ActionMailer::Base.email(:from => @email,
+			:to => 'eaton.cw@gmail.com',
+			:subject => "A new contact form message from #{@name}",
+			:body => @message).deliver_now
+	end
 end
