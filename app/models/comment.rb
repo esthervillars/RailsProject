ca41f2d46374aada86f62de3f904abf355b2cbb1
Christopher Eaton <eaton.cw@gmail.com>
2016-08-19 15:58:20 -0700

    try try

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,14 +2,13 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
-  validates :body, presence: true
+  
 
   #potentially add conditional to rating if comment model is used for other purposes, like users sending each other messages.
-  validates :rating, numericality: { only_integer: true }
+  
 
   #validate relationship, not database field of foreign
-  validates :user, presence: true
-  validates :product, presence: true
+  
   
 
   scope :rating_desc, -> { order(rating: :desc) }
