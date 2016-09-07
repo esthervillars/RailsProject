12cee02874057ec0c6b7a10dcfd4857995963547
Christopher Eaton <eaton.cw@gmail.com>
2016-09-07 12:02:40 -0700

    indent check

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments, as: :commentable
 	
-
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :img_url, presence: true
 	validates :body, presence: true
