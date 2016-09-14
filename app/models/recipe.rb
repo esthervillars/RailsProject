3ac55b4f320043558a16fd6f71f4242cec4b2c9b
Christopher Eaton <eaton.cw@gmail.com>
2016-09-14 15:42:19 -0700

    updated indentation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,17 +1,17 @@
 class Recipe < ActiveRecord::Base
 
-belongs_to :user
-has_many :comments, as: :commentable
-has_many :ingredients
-accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
-															
+	belongs_to :user
+	has_many :comments, as: :commentable
+	has_many :ingredients
+	accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+																
 
-has_many :directions
-accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
+	has_many :directions
+	accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
 
-validates :title, :description, :image, presence: true
+	validates :title, :description, :image, presence: true
 
-has_attached_file :image, styles: { medium: "400x400#", large: "1200x300#"}
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	has_attached_file :image, styles: { medium: "400x400#", large: "1200x300#"}
+	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
