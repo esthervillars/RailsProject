73694340a3dee9f8cf786e6b0177cbead7a012bb
Christopher Eaton <eaton.cw@gmail.com>
2016-09-14 16:12:26 -0700

    new sublime text

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,13 +7,13 @@ class Ability
 		user ||= User.new # guest user (not logged in)
 		
 		if user.admin?
-				can :manage, :all
+			can :manage, :all
 		else
-				can :manage, User, id: user.id
-				can :manage, Order, id: user.id
-				can :read, Comment, id: user.id
+			can :manage, User, id: user.id
+			can :manage, Order, id: user.id
+			can :read, Comment, id: user.id
 		end
-				
+
 		#:manage is any action available for user, second parameter is restriction - user must have same ID as the user record
 		
 		#   if user.admin?
